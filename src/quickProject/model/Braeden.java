c274f05483bb8b7af4a0851f137229a5e83e82b4
kmart22 <kmar7070@740s-pl-111.local>
2017-03-24 08:36:24 -0600

    created a abstract class and then another that extends it

---
+++ src/quickProject/model/Braeden.java
@@ -0,0 +1,12 @@
+package quickProject.model;
+
+public class Braeden extends StudentAthlete
+{
+
+	public Braeden()
+	{
+		super("Braeden", 5);
+	}
+	
+	
+}
---
+++ src/quickProject/model/StudentAthlete.java
@@ -0,0 +1,12 @@
+package quickProject.model;
+
+public abstract class StudentAthlete
+{
+	String name;
+	int speed;
+	
+	public StudentAthlete(String name, int speed)
+	{
+		
+	}
+}
